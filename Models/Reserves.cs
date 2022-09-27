5cbc7b930269e87b134d6676fb7132d083a99689
korerima <samuel.kiros.sk9701@gmail.com>
2022-09-27 09:48:45 +0300

    reserves and equb order changes

--- Models/Reserves.cs
+++ Models/Reserves.cs
@@ -7,11 +7,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace equb.Models
 {
-    public class Equb_order
+    public class Reserves
     {
         [Key]
-        [Display(Name = "Equb order ID")]
-        public int equb_order_id { get; set; }
+        [Display(Name = "Reservation ID")]
+        public int reserve_id { get; set; }
 
         [Display(Name = "User ID")]
         public int cust_id { get; set; }
@@ -22,18 +22,8 @@ namespace equb.Models
         public Equb_detail Equb_Detail { get; set; }
 
         [Display(Name = "Order Number")]
-        [Range(0.00, 50.00, ErrorMessage = "The order placement should't be greater than 50 and less than 0")]
-        [Required(ErrorMessage = "Winning order is required")]
         public int order { get; set; }
 
-        [Display(Name = "Order Status")]
-        [Required(ErrorMessage = "Order status is required")]
-        public string status { get; set; }
-
-        [Required(ErrorMessage = "Win date is required")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Win Date")]
-        public DateTime win_date { get; set; }
     }
+    
 }
